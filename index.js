59f522bb4fccbefbeab453dfa8d9a8a2992a8c52
Carsen Asterino <carsenasterino@gmail.com>
2022-02-15 01:46:34 -0700

    Start repo from local version

---
+++ index.js
@@ -0,0 +1,43 @@
+// Init the start function
+window.onload = function()
+{
+  Start();
+}
+
+function Start()
+{
+  fetch('data.json').then(response => response.json()).then(data => console.log(data));
+}
+
+// First function called by Google
+function initMap() {
+  // Asking permission for location and checking browser capability
+  if (navigator.geolocation)
+  {
+      navigator.geolocation.getCurrentPosition(sucessLocation);
+  } else 
+  {
+      alert('Your browser does not support location services.')
+  }
+}
+
+// Sucess function for getting permission, finding lat and long of user, using Google API to init the map
+function sucessLocation(pos)
+{
+  var lat = pos.coords.latitude;
+  var long = pos.coords.longitude;
+  const home = { lat: lat, lng: long };
+
+  // Center the map at the home point determined
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: home,
+    disableDefaultUI: true,
+  });
+
+  // Place a marker at the home point
+  const marker = new google.maps.Marker({
+    position: home,
+    map: map,
+  });
+}
