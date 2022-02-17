aa55f07e18daf15043eacee0aa1736ea333c4744
Carsen Asterino <carsenasterino@gmail.com>
2022-02-17 11:24:16 -0700

    Fixed bug where data const needed to be init

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const data;
+const data = {};
 getLocationData();
 
 async function getLocationData()
