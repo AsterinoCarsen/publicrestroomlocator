594f8a84879d1e25519f72977ae5136e721d4c6e
Carsen Asterino <carsenasterino@gmail.com>
2022-02-19 17:43:24 -0700

    Seperated maps API from Firebase scripts

---
+++ fireBaseInit.js
@@ -0,0 +1,26 @@
+console.debug("is running");
+
+import { initializeApp } from '/firebase/app';
+import { getFirestore, collection, getDocs } from '/firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPtH7jVN4wNCKOyUf6p4QfpX3UQXDydZ4",
+  authDomain: "publicrestroomlocator.firebaseapp.com",
+  databaseURL: "https://publicrestroomlocator-default-rtdb.firebaseio.com",
+  projectId: "publicrestroomlocator",
+  storageBucket: "publicrestroomlocator.appspot.com",
+  messagingSenderId: "660860024187",
+  appId: "1:660860024187:web:9439b3959865ffd80eb9de"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const data = {};
+getLocationData();
+
+async function getLocationData()
+{
+  data = collection(db, 'locations');
+  console.debug(data);
+}
--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-import { initializeApp } from '/firebase/app';
-import { getFirestore, collection, getDocs } from '/firebase/firestore/lite';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBPtH7jVN4wNCKOyUf6p4QfpX3UQXDydZ4",
-  authDomain: "publicrestroomlocator.firebaseapp.com",
-  databaseURL: "https://publicrestroomlocator-default-rtdb.firebaseio.com",
-  projectId: "publicrestroomlocator",
-  storageBucket: "publicrestroomlocator.appspot.com",
-  messagingSenderId: "660860024187",
-  appId: "1:660860024187:web:9439b3959865ffd80eb9de"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-const data = {};
-getLocationData();
-
-async function getLocationData()
-{
-  data = collection(db, 'locations');
-  console.debug(data);
-}
-
 // First function called by Google
 function initMap() {
   // Asking permission for location and checking browser capability
